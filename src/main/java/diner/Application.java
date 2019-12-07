6fcf846ab83e932c77d771b511603cd3c971073b
silviu_alex <sabau.silviu.bv@gmail.com>
2019-12-07 15:42:08 +0200

    Commit

--- src/main/java/diner/Application.java
+++ src/main/java/diner/Application.java
@@ -2,10 +2,11 @@ package diner;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 
 import diner.Application;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = { SecurityAutoConfiguration.class })
 public class Application {
 
   public static void main(final String[] args) 
